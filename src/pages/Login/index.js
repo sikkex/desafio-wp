3645d5595a5602629b2835dd9c05ad3d8058530f
sikkex <daniel_mattos@id.uff.br>
2019-12-18 06:10:32 -0300

    tirando verificação por email

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -15,7 +15,7 @@ export default class Login extends Component {
       firebase.auth.GoogleAuthProvider.PROVIDER_ID,
       firebase.auth.FacebookAuthProvider.PROVIDER_ID, 
       firebase.auth.GithubAuthProvider.PROVIDER_ID,
-      firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      // firebase.auth.EmailAuthProvider.PROVIDER_ID,
     ],
     callbacks: {
       signInSuccess: () => false
